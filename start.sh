ca44c412659b4af51b3927fa68b8b8e340c5a893
NeedKwok <1669677029@qq.com>
2019-06-08 00:48:44 +0800

    lgc swiming in the magma

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -Xms1024m -Xmx1024m -jar /root/forge-1.12.2-14.23.5.2838-universal.jar nogui;
+java -Xms2048m -Xmx3096m -jar /root/mc/forge-1.12.2-14.23.5.2838-universal.jar nogui;
